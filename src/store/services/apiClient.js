87b521ae95f100b6780e58f98f9c8e38df178025
07Akashh <aonecreations1@gmail.com>
2024-12-02 16:07:59 +0530

    fix: time exceed error

--- src/store/services/apiClient.js
+++ src/store/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
     baseURL: 'https://auth-page-backend-xglx.onrender.com/api',
-    timeout: 10000,
+    // timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
